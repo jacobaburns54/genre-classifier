891d7031d365479fd3460d7563a7872d22f54f79
Jacob Ellington <cannotilever@protonmail.com>
2024-11-24 13:43:16 -0500

    add memory tracing

--- src/classifier.py
+++ src/classifier.py
@@ -3,6 +3,7 @@ from youtubeSupport import youtube_get
 import random
 import time
 import os
+import tracemalloc
 def run_comparison(function):
     pass
 
@@ -11,19 +12,26 @@ def random_guess(wav):
     return random.choice(g),(1 / len(g))
 
 def main():
-    size = os.get_terminal_size()
-    os.system("clear")
-    print(" Welcome to the Genre Classifier! ".center(size.columns,"="))
-    link = input("Enter a youtube link: ")
-    print("\nLoading audio...")
-    #call link-to-wav fcn
-    wav = youtube_get(link)
-    methods = [("CRNN",guesserate), ("Random",random_guess)]
-    print("\nLoading done. Starting evaluation...")
-    for name, m in methods:
-        start = time.time()
-        (genre, certainty) = m(wav)
-        print("{}: Song is {} with {}% certainty. (took {}s)".format(name, genre, int(100*certainty), round(time.time()-start,3)))
-    os.remove(wav)
+    while True:
+        size = os.get_terminal_size()
+        os.system("clear")
+        print(" Welcome to the Genre Classifier! ".center(size.columns,"="))
+        link = input("Enter a youtube link: ")
+        print("\nLoading audio...")
+        #call link-to-wav fcn
+        wav = youtube_get(link)
+        methods = [("CRNN",guesserate), ("Random",random_guess)]
+        print("\nLoading done. Starting evaluation...")
+        for name, m in methods:
+            start = time.time()
+            tracemalloc.start()
+            (genre, certainty) = m(wav)
+            current, peak = tracemalloc.get_traced_memory()
+            tracemalloc.stop()
+            print("\n{} is {}% certain song is of genre {}.".format(name, int(100*certainty), genre))
+            print("Took {}s and used {}MB of memory.\n".format(round(time.time()-start,3), round(peak / (1024 ** 2),3)))
+        os.remove(wav)
+        if input("Run again? (Y/n): ").lower() == "n":
+            break
 if __name__ == "__main__":
     main()
